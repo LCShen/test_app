6484c864f156aa6032bebfa91b826b2445a5a1e2
LCShen <william@Williams-RMBP.local>
2014-08-12 00:07:17 +0800

    Fix some compile problem

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-TestApp::Aapplication.routes.draw do
+TestApp::Application.routes.draw do
   get "static_pages/home"
   get "static_pages/help"
   # The priority is based upon order of creation: first created -> highest priority.

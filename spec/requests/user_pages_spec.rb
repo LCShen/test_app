9e7ed3ae4c46eec5a1d7a69f9bb09fd5bd7d770c
LCShen <william@Williams-RMBP.local>
2014-08-17 13:48:51 +0800

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -21,6 +21,53 @@ describe "User pages" do
     it { should have_content('Sign Up') }
     it { should have_title(full_title('Sign Up')) }
 
+    describe "signup" do
+
+      before { visit signup_path }
+      let(:submit) { "Create my account" }
+
+      describe "with invalid information" do
+
+        it "should not create a user" do
+          expect { click_button submit }.not_to change(User, :count)
+        end
+
+        describe "after submission" do
+          before { click_button submit }
+          it { should have_title('Sign Up') }
+          it { should have_content('error') }
+        end
+
+      end
+
+      describe "with valid information" do
+
+        before do
+
+          fill_in "Name", with: "Example User"
+          fill_in "Email", with: "user@example.com"
+          fill_in "Password", with: "foobar"
+          fill_in "Confirmation", with: "foobar"
+
+        end
+
+          describe "after saving the user" do
+            before { click_button submit }
+            let(:user) { User.find_by(email: 'user@example.com') }
+
+            it { should have_link('Sign Out') }
+            it { should have_title(user.name) }
+            it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+          end
+
+        it "should create a user" do
+          expect { click_button submit }.to change(User, :count).by(1)
+        end
+
+      end
+
+    end
+
   end
 
 end

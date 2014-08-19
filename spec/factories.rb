b91d7f1466a764c45bddbdd962ac9bc6b01ab204
LCShen <william@Williams-RMBP.local>
2014-08-19 20:46:49 +0800

    Add user microposts

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
   factory :user do
+
     sequence(:name) { |n| "Person #{n}" }
     sequence(:email) { |n| "person_#{n}@example.com"}
     password "foobar"
@@ -8,5 +9,10 @@ FactoryGirl.define do
     factory :admin do
       admin true
     end
+  end
+
+  factory :micropost do
+    content "Lorem ipsum"
+    user
   end
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -77,6 +77,24 @@ describe "Authentication" do
             expect(page).to have_title('Edit user')
 
           end
+
+          describe "when signing in again" do
+
+            before do
+
+              delete signout_path
+              visit signin_path
+              fill_in "Email", with: user.email
+              fill_in "Password", with: user.password
+              click_button "Sign In"
+
+            end
+
+            it "should render the default (profile) page" do
+
+              expect(page).to have_title(user.name)
+            end
+          end
         end
       end
 
@@ -99,6 +117,23 @@ describe "Authentication" do
         describe "visiting the user index" do
           before { visit users_path }
           it { should have_title('Sign In') }
+        end
+      end
+
+      describe "in the Microposts controller" do
+
+        describe "submitting to the create action" do
+
+          before { post microposts_path }
+          specify { expect(response).to redirect_to(signin_path) }
+
+        end
+
+        describe "submitting to the destroy action" do
+
+          before { delete micropost_path(FactoryGirl.create(:micropost)) }
+          specify { expect(response).to redirect_to(signin_path) }
+
         end
       end
     end
